79fc952847fbe48104024ed4f6370252f36b43df
Sathira Sri Sathara <sathira.nirmal@gmail.com>
2025-04-28 01:08:25 +0530

    Added favicon and updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,13 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Niolla Pvt Ltd - Smart IT Solutions",
+  title: "Niolla - Smart IT Solutions",
   description:
     "Empowering businesses with smart IT solutions. Specializing in business automation, point-of-sales systems, and custom software development.",
-    generator: 'v0.dev'
+    generator: 'v0.dev',
+    icons: {
+      icon: '/favicon.png', // or .png if you prefer
+    },
 }
 
 export default function RootLayout({
